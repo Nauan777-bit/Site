26259817facf479dd15596ea97a04058bd5ec308
Nauan777-bit <nauanpietro41@gmail.com>
2025-05-16 14:23:38 -0300

    Update particles-config.js

--- particles-config.js
+++ particles-config.js
@@ -1,45 +1,61 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 80,
+      "value": 100,
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "#ffffff"
+      "value": "#00ffff"
     },
     "shape": {
       "type": "circle"
     },
     "opacity": {
-      "value": 0.4,
+      "value": 0.6,
       "random": true
     },
     "size": {
-      "value": 3,
+      "value": 2.5,
       "random": true
     },
     "line_linked": {
       "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
+      "distance": 140,
+      "color": "#00ffff",
       "opacity": 0.2,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 2,
+      "speed": 1.5,
       "direction": "none",
-      "out_mode": "out"
+      "out_mode": "bounce"
     }
   },
   "interactivity": {
+    "detect_on": "canvas",
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "repulse"
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      }
+    },
+    "modes": {
+      "grab": {
+        "distance": 200,
+        "line_linked": {
+          "opacity": 0.35
+        }
+      },
+      "push": {
+        "particles_nb": 4
       }
     }
   },
